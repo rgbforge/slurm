68b8b4300880049ee7f27be26c7208d8435b8653
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-07-12 18:44:40 -0500

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -2,4 +2,4 @@
 # repo and package install
 #---------------------------------------------------------------
 dnf config-manager --set-enabled crb
-dnf install munge munge-devel pam-devel perl readline-devel dbus-devel mariadb-server mariadb-devel rpm-build jq
+dnf -y install munge munge-devel pam-devel perl readline-devel dbus-devel mariadb-server mariadb-devel rpm-build jq
