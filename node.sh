cbb003b373b2343e19382a614ee5559dbacbd153
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-07-12 16:11:21 -0500

    Update node.sh

--- node.sh
+++ node.sh
@@ -5,10 +5,9 @@ wget https://download.schedmd.com/slurm/slurm-23.02.5.tar.bz2
 mv slurm-23.02.5.tar.bz2 /root
 cd /root 
 rpmbuild -ta slurm-23.02.5.tar.bz2
-echo "FIX *.RPM"
-read varname
+rm rpmbuild/RPMS/x86_64/slurm-torq*
+rm rpmbuild/RPMS/x86_64/slurm-pam*
 rpm --install rpmbuild/RPMS/x86_64/*.rpm
-echo "FIX slurm.conf"
 
 
 #---------------------------------------------------------------
@@ -21,7 +20,6 @@ useradd  -m -c "Slurm workload manager" -d /var/lib/slurm -u $SlurmUSER -g slurm
 #---------------------------------------------------------------
 # create and modify slurmd log files, start slurmd
 #---------------------------------------------------------------
-#clush -bw <node-list> --copy /etc/slurm/slurm.conf --dest /etc/slurm/slurm.conf
 mkdir /var/spool/slurmd /var/log/slurm
 chown slurm: /var/spool/slurmd  /var/log/slurm
 chmod 755 /var/spool/slurmd  /var/log/slurm
@@ -29,7 +27,7 @@ touch /var/log/slurm/slurmd.log
 chown slurm: /var/log/slurm/slurmd.log
 slurmd -C
 echo "FIX slurm.conf part3"
-read varname
+read
 #NodeName=test001 Boards=1 SocketsPerBoard=2 CoresPerSocket=2 ThreadsPerCore=1 RealMemory=8010 TmpDisk=32752 Feature=xeon
 #TmpFS=/scratch
 sudo firewall-cmd --permanent --add-port=6817/tcp
@@ -37,4 +35,3 @@ sudo firewall-cmd --reload
 systemctl enable slurmd.service
 systemctl start slurmd.service
 systemctl status slurmd.service
-
