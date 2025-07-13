b425dd34d4c25eb613449f362dffc756f5cd81b1
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-07-12 20:09:30 -0500

    Update slurm_clush.sh

--- slurm_clush.sh
+++ slurm_clush.sh
@@ -13,22 +13,22 @@ CONTROLLER_HOSTNAME=$(jq -r '.host.name' "$JSON_FILE")
 ALL_NODES=$(jq -r '.nodes[].name' "$JSON_FILE" | tr '\n' ' ' | sed 's/ $//')
 COMPUTE_NODES=$(jq -r --arg ctrl "$CONTROLLER_HOSTNAME" '.nodes[] | select(.name != $ctrl) | .name' "$JSON_FILE" | tr '\n' ',' | sed 's/,$//')
 
-wget -q --show-progress https://download.schedmd.com/slurm/slurm-${SLURM_VERSION}.tar.bz2
+wget --show-progress https://download.schedmd.com/slurm/slurm-${SLURM_VERSION}.tar.bz2
 mv slurm-${SLURM_VERSION}.tar.bz2 /root/
 cd /root/
-rpmbuild -ta slurm-${SLURM_VERSION}.tar.bz2 &>/dev/null
+rpmbuild -ta slurm-${SLURM_VERSION}.tar.bz2
 rm -f rpmbuild/RPMS/x86_64/slurm-torque-*
 rm -f rpmbuild/RPMS/x86_64/slurm-pam-*
 
 if [ -n "$COMPUTE_NODES" ]; then
-    clush -w $COMPUTE_NODES --copy /root/rpmbuild/RPMS/x86_64/ --dest /root/slurm_rpms &>/dev/null
+    clush -w $COMPUTE_NODES --copy /root/rpmbuild/RPMS/x86_64/ --dest /root/slurm_rpms
 fi
 
 clush -w "$ALL_NODES" "bash -c '
-rpm --install --nodeps /root/slurm_rpms/*.rpm &>/dev/null
+rpm --install --nodeps /root/slurm_rpms/*.rpm
 export SLURMUSER=1001
-groupadd -g \$SLURMUSER slurm &>/dev/null
-useradd -m -c \"Slurm workload manager\" -d /var/lib/slurm -u \$SLURMUSER -g slurm -s /bin/bash slurm &>/dev/null
+groupadd -g \$SLURMUSER slurm
+useradd -m -c \"Slurm workload manager\" -d /var/lib/slurm -u \$SLURMUSER -g slurm -s /bin/bash slurm
 mkdir -p /var/spool/slurmd /var/log/slurm
 chown slurm: /var/spool/slurmd /var/log/slurm
 chmod 755 /var/spool/slurmd /var/log/slurm
@@ -36,16 +36,15 @@ touch /var/log/slurm/slurmd.log
 chown slurm: /var/log/slurm/slurmd.log
 '"
 
-dd if=/dev/urandom of=$MUNGE_KEY_FILE bs=1 count=1024 &>/dev/null
+dd if=/dev/urandom of=$MUNGE_KEY_FILE bs=1 count=1024
 chown munge: $MUNGE_KEY_FILE
 chmod 400 $MUNGE_KEY_FILE
-clush -w $ALL_NODES --copy $MUNGE_KEY_FILE --dest $MUNGE_KEY_FILE &>/dev/null
-clush -w $ALL_NODES "chown munge: $MUNGE_KEY_FILE && chmod 400 $MUNGE_KEY_FILE && systemctl restart munge" &>/dev/null
+clush -w $ALL_NODES --copy $MUNGE_KEY_FILE --dest $MUNGE_KEY_FILE
+clush -w $ALL_NODES "chown munge: $MUNGE_KEY_FILE && chmod 400 $MUNGE_KEY_FILE && systemctl restart munge"
 
 NODE_INFO_FILE=$(mktemp)
 clush -w $ALL_NODES "slurmd -C" > $NODE_INFO_FILE
 
-jq -r '.host.name' "$JSON_FILE" > /dev/null
 while IFS= read -r line; do
     if [[ $line =~ NodeName=([^[:space:]]+) ]]; then
         NODE_NAME=${BASH_REMATCH[1]}
@@ -73,8 +72,8 @@ jq -r '.nodes[] | .name + "|" + .ip + "|" + .config' "$JSON_FILE" | while IFS="|
   fi
 done
 
-clush -w $ALL_NODES --copy $SLURM_CONF_FILE --dest $SLURM_CONF_FILE &>/dev/null
+clush -w $ALL_NODES --copy $SLURM_CONF_FILE --dest $SLURM_CONF_FILE
 
 systemctl restart slurmctld
-systemctl status
-clush -w $ALL_NODES "systemctl enable --now slurmd" &>/dev/null
+systemctl status slurmctld
+clush -w $ALL_NODES "systemctl enable --now slurmd"
