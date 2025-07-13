a0aca755abe8ef4e445b492fed681e1dde99690d
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-07-12 21:22:53 -0500

    Delete preinstall.sh

--- preinstall.sh
+++
@@ -1,6 +0,0 @@
-#---------------------------------------------------------------
-# repo and package install
-#---------------------------------------------------------------
-dnf config-manager --set-enabled crb
-dnf -y install epel-release
-dnf -y install munge munge-devel pam-devel perl readline-devel dbus-devel mariadb-server mariadb-devel rpm-build jq clustershell
